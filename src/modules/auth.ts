80dbfd25ea2afbeae0aafc780a691db80976cdbe
Henry Hein <henry@regentmarkets.com>
2024-04-12 14:17:18 +0800

    initial comit

---
+++ src/modules/auth.ts
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+export const createJWT = (user) => {
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET
+  );
+
+  return token;
+};
+
+export const protect = (req, res, next) => {
+  const bearer = req.headers.authorization;
+
+  if(!bearer) {
+    res.status(401);
+    res.send({message: 'not authorized.'})
+    return;
+  }
+
+  
+
+}
---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import router from './router';
+import morgan from 'morgan';
+import { protect } from './modules/auth';
+
+const app = express();
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+  res.status(200);
+  res.json({message: 'k'});
+});
+
+app.use('/api', protect, router);
+export default app;
